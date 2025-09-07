3847a505680942e845e3d45c413098aa2e666442
madisoncavill <cavillmadison@gmail.com>
2025-09-07 15:23:45 -0700

    Initial Commit

---
+++ GreenhouseStudyBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GreenhouseStudyBuddy
+//
+//  Created by Madison Cavill on 2025-09-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreenhouseStudyBuddy/GreenhouseStudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenhouseStudyBuddyApp.swift
+//  GreenhouseStudyBuddy
+//
+//  Created by Madison Cavill on 2025-09-07.
+//
+
+import SwiftUI
+
+@main
+struct GreenhouseStudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
